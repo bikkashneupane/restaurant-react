6b90845034fa6b56d76b9be4d9f5161f3ba269f0
bikashneupane17 <edubikkash@gmail.com>
2024-10-31 16:17:50 +1100

    React Component test cases

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,3 +1,28 @@
 export const ContactUs = () => {
-  return <div>Contact Use</div>;
+  return (
+    <div className="p-2 flex justify-center items-center gap-1 flex-col w-full">
+      <h1 className="font-bold text-2xl">Contact Use Page</h1>
+
+      <form action="" className="flex flex-col gap-2  w-1/3">
+        <input
+          type="text"
+          placeholder="Name"
+          className="border px-4 py-2 rounded"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border px-4 py-2 rounded"
+        />
+        <textarea
+          rows={5}
+          placeholder="Message"
+          className="border px-4 py-2 rounded"
+        />
+        <button className="border rounded px-4 py-2 bg-blue-700 text-white">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
